9ab4b4584bdf2809ea0b535483ba9093a4d638f1
Daniil Rose <daniil.rose@posteo.org>
2023-07-22 13:27:58 -0400

    Remove instruction_info.rs and update formatting for parse

--- src/assembler/mod.rs
+++ src/assembler/mod.rs
@@ -1,3 +1,5 @@
+use log::info;
+
 pub fn assemble(program: &str) -> Vec<u8> {
     let lines = program.lines().map(|x| x.trim()).collect::<Vec<&str>>();
     let mut bytes = vec![];
@@ -11,26 +13,30 @@ pub fn assemble(program: &str) -> Vec<u8> {
         }
     }
 
+    info!("The following is in bytes: {:?}", bytes);
+
     bytes
 }
 
-    fn parse(line: &str) -> (u8, Vec<u8>) {
-        let parts: Vec<&str> = line.split_whitespace().collect();
-        
-        let opcode = match parts[0] {
-            "LDA" => 0xA9,
-            "TAX" => 0xAA,
-            "INX" => 0xE8,
-            _ => 0x00
-        };
-        
-        let mut args = vec![];
-        
-        if parts.len() > 1 {
-            if let Ok(value) = u8::from_str_radix(&parts[1][2..], 16) {
-                args.push(value); 
-            }
+fn parse(line: &str) -> (u8, Vec<u8>) {
+    let parts: Vec<&str> = line.split_whitespace().collect();
+
+    info!("The parts parsed are: {:?}", parts);
+
+    let opcode = match parts[0] {
+        "LDA" => 0xA9,
+        "TAX" => 0xAA,
+        "INX" => 0xE8,
+        _ => 0x00,
+    };
+
+    let mut args = vec![];
+
+    if parts.len() > 1 {
+        if let Ok(value) = u8::from_str_radix(&parts[1][2..], 16) {
+            args.push(value);
         }
-        
-        (opcode, args)
     }
+
+    (opcode, args)
+}
